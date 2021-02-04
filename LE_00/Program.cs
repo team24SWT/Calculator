faa37b9d4836f46b72bcf370093ac8189d698321
Simon Shahrour <au624634@uni.au.dk>
2021-02-04 13:50:42 +0100

    Console Write linje 13

--- LE_00/Program.cs
+++ LE_00/Program.cs
@@ -10,6 +10,7 @@ namespace LE_00
 
             Console.WriteLine($"Result is{Beregning1.Add(5, 6)}");
             Console.WriteLine($"Result is{Beregning1.Power(3, 5)}");
+            Console.WriteLine("Ole is the President"); 
         }
     }
 }
